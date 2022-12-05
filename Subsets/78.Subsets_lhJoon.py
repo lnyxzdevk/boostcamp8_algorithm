7ca9da54f193914ef225ab99d298d0f1ab6525dd
lhJoon <87929279+coderJoon@users.noreply.github.com>
2022-12-05 21:26:15 +0900

    Update 78.Subsets_lhJoon.py

--- Subsets/78.Subsets_lhJoon.py
+++ Subsets/78.Subsets_lhJoon.py
@@ -1,5 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        """
+        nums : [1,2,3]
+        tmp : []
+        length : 0
+        
+              i=0    i=1      i=2     i=3 : len(nums)                                              i=3 : len(nums)
+        [] -> [1] -> [1,2] -> [1,2,3] -> results.append([1,2,3]) -> return -> BT(i:2,tmp:[1,2]) -> results.append([1,2]) -> ...
+        """
         results = []
         def BT(length,tmp):
             if length == len(nums):
