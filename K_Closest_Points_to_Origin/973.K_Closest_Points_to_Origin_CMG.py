cc19f1ef078bf6d2c25d639eb0ae8aac87432831
조문기_T4207 <siryuon@gmail.com>
2022-12-20 12:11:23 +0900

    Add files via upload

---
+++ K_Closest_Points_to_Origin/973.K_Closest_Points_to_Origin_CMG.py
@@ -0,0 +1,15 @@
+import heapq
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap, result = [], []
+
+        for x, y in points:
+            dist = x**2 + y**2
+            heapq.heappush(heap, [dist, x, y])
+
+        for _ in range(k):
+            dist, x, y = heapq.heappop(heap)
+            result.append([x, y])
+        
+        return result
