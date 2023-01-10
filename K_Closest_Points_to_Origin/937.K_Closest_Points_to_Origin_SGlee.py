015c6a64c01ea17e0b2ccf9fe4eada0bf10597b8
LEE, Seonggu <dltjdrn1123@gmail.com>
2023-01-10 03:24:15 +0000

    delete

--- K_Closest_Points_to_Origin/937.K_Closest_Points_to_Origin_SGlee.py
+++
@@ -1,8 +0,0 @@
-
-class Solution:
-    def get_dist(self, point):
-        return point[0]**2 + point[1]**2
-
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        k_closest = sorted([(self.get_dist(p), p) for p in points])[:k]
-        return list(map(lambda x: x[1], k_closest))
